2ca6aa80c3ec607136fc5a78404187f38692ed9c
rragewrath <rragewrath@gmail.com>
2025-03-23 11:16:36 +0700

    Initial commit

---
+++ game.hpp
@@ -0,0 +1,82 @@
+#ifndef Game_hpp
+#define Game_hpp
+
+#include<SDL.h>
+#include<SDL_image.h>
+#include<bits/stdc++.h>
+#include <SDL_mixer.h>
+#include<SDL_ttf.h>
+
+const double land = 400;
+
+class Game {
+    public:
+        Game();
+        ~Game();
+
+        void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+        void gameEvents();
+        void update();
+        void render();
+        void clean();
+
+        bool running(){
+            return isRunning;
+        }
+    //800, 600
+    private:
+        bool isRunning;
+        SDL_Window *window;
+        SDL_Renderer *renderer;
+        double xPlayer = 0;
+        double yPlayer = land;
+        int score = 0;
+
+        SDL_Texture *background;
+
+        Mix_Chunk *harmSound;
+        Mix_Chunk *gameOver;
+        Mix_Chunk *dashSound;
+        Mix_Chunk *skillSound;
+        Mix_Chunk *windwallSound;
+        Mix_Chunk *takescoreSound;
+        Mix_Chunk *CD;
+        Mix_Chunk *heal;
+        Mix_Chunk *speedboost;
+        Mix_Chunk *jump;
+        Mix_Chunk *block;
+
+        Mix_Music *music;
+
+        SDL_Texture *walk[11][2];
+        SDL_Texture *book[13];
+        SDL_Texture *b[13];
+        SDL_Texture *skill[13];
+        SDL_Texture *sword;
+        SDL_Texture *sword2;
+        SDL_Texture *cd;
+        SDL_Texture *angry1;
+        SDL_Texture *angry2;
+        SDL_Texture *blood;
+        SDL_Texture *ghost1;
+        SDL_Texture *ghost2;
+        SDL_Texture *dash_particle;
+        SDL_Texture *dash;
+        SDL_Texture *killangry;
+        SDL_Texture *windwall;
+        SDL_Texture *shielded;
+        SDL_Texture *killghost;
+        SDL_Texture *Chest;
+        SDL_Texture *harm;
+        SDL_Texture *shield;
+        SDL_Texture *heart;
+        SDL_Texture *heart2;
+        SDL_Texture *takescore;
+        SDL_Texture *speed;
+        SDL_Texture *speed2;
+        SDL_Texture *HP;
+        TTF_Font *font;
+};
+
+#endif // Game_hpp
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "game.hpp"
+
+Game *game = nullptr;
+
+int main(int argc, char* argv[]){
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+    int frameStart, frameTime;
+
+    game = new Game();
+
+    game -> init("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+
+    while(game -> running()){
+        frameStart = SDL_GetTicks();
+
+        game -> gameEvents();
+        game -> update();
+        game -> render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if(frameDelay > frameTime){
+            SDL_Delay(frameDelay - frameTime);
+        }
+    }
+
+    game -> clean();
+
+    return 0;
+}
