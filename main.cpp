c4615574f98ed0daf2dfc3b68dc755abcded23d6
rragewrath <rragewrath@gmail.com>
2025-05-05 19:33:49 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ Game *game = nullptr;
 
 int main(int argc, char* argv[]){
     const int FPS = 60;
-    const int frameDelay = 10;
+    const int frameDelay = 1000 / FPS;
     int frameStart, frameTime;
 
     game = new Game();
